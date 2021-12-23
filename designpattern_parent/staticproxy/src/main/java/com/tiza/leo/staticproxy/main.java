735ee567cb5cb470412af3d1f69e4f7eba2e51ce
wei198621 <wei198621@126.com>
2021-12-23 23:35:06 +0800

    sa

---
+++ designpattern_parent/staticproxy/src/main/java/com/tiza/leo/staticproxy/main.java
@@ -0,0 +1,13 @@
+package com.tiza.leo.staticproxy;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * @author leowei
+ * @date 2021/12/23  - 23:23
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
